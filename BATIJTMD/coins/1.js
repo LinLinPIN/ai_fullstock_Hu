e1b2427cd6c88526113102260bde9aa653d7161e
LinLinPIN <1033979218@qq.com>
2023-11-28 17:19:52 +0800

    dp

---
+++ BATIJTMD/coins/1.js
@@ -0,0 +1,16 @@
+const coinChange = function (coins,amount){
+    const f = []
+    f[0] = 0
+    for(let i=1;i<=amount;i++){
+        f[i] = Infinity
+        for(let j=0 ;j<coins.length();j++)
+       { 
+        if(i-coins[j]>=0){
+        f[i]=Math.min(f[i],f[i-coins[j]]+1);
+       }
+    }
+    }
+    if(f[amount]===Infinity)
+    return -1
+    else return f[amount]   
+}
