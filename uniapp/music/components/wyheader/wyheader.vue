f7803a5efcef13d30a26ec17d8de65056ee383bd
LinLinPIN <1033979218@qq.com>
2024-01-19 17:30:12 +0800

    wyy

---
+++ uniapp/music/components/wyheader/wyheader.vue
@@ -0,0 +1,22 @@
+<template>
+	<view class="header">
+		<uni-icons type="bars" size="30"></uni-icons>
+			<slot name="content"></slot>
+		<uni-icons type="mic" size="30"></uni-icons>
+	</view>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+.header{
+	height: 100rpx;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding: 0 15rpx;
+
+}
+</style>
