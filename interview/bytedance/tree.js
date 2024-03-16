dd7770e1aca99bb3d5bf5cff4e65b8f74b56ca1c
LinLinPIN <1033979218@qq.com>
2024-03-16 16:57:34 +0800

    数组转树

---
+++ interview/bytedance/tree.js
@@ -0,0 +1,68 @@
+// 新手在上班后会遇到的编程难题
+// 树状列表 文件夹 省市区联动
+// 列表转树
+// parentId 树
+const list = [
+    {
+        id: 1001,
+        parentId: 0,
+        name: "AA",
+    },
+    {
+        id: 1002,
+        parentId: 1001,
+        name: "BB",
+    },
+    {
+        id: 1003,
+        parentId: 1001,
+        name: "CC",
+    },
+    {
+        id: 1004,
+        parentId: 1003,
+        name: "DD",
+    },
+    {
+        id: 1005,
+        parentId: 1003,
+        name: "EE",
+    },
+    {
+        id: 1006,
+        parentId: 1002,
+        name: "FF",
+    },
+    {
+        id: 1007,
+        parentId: 1002,
+        name: "GG",
+    },
+    {
+        id: 1008,
+        parentId: 1004,
+        name: "HH",
+    },
+    {
+        id: 1009,
+        parentId: 1005,
+        name: "II",
+    },
+];
+
+
+function listToTree(data) {
+    const res = []
+    data.forEach(item => {
+        const parentId = data.find(node => node.id === item.parentId)
+        if (parentId) {
+            parentId.children = parentId.children || []
+            parentId.children.push(item)
+        } else {
+            res.push(item)
+        }
+    });
+    return res;
+}
+
+console.log(listToTree(list));
